9d35a8f0399f1a1299b5522e909799c2434d21c8
Aldana Quintana <aldanaqm@gmail.com>
2015-03-12 22:31:27 -0300

    Restrict new javascript to one view

--- app/assets/javascripts/rails_views/user_registration/new.js
+++ app/assets/javascripts/rails_views/user_registration/new.js
@@ -1,26 +1,28 @@
 $(document).ready(function(){
-  var group_select = $("#group_select");
-  var groups = $("#groups-container").find("[data-groups]").data("groups");
+  if($("#finish_registration").length == 1){
+    var group_select = $("#group_select");
+    var groups = $("#groups-container").find("[data-groups]").data("groups");
 
-  var emptyGroups = function(){
-    group_select[0].innerHTML = "";
-  }
+    var emptyGroups = function(){
+      group_select[0].innerHTML = "";
+    }
 
-  var fillGroups = function(selected_course_id){
-    var groups_of_course = _.select(groups, function(group){
-      return group.course_id.toString() === selected_course_id;
-    })
-    _.each(groups_of_course, function(group){
-      group_select[0].appendChild(new Option(group.values[0], group.values[1].toString()))
-    })
-  }
+    var fillGroups = function(selected_course_id){
+      var groups_of_course = _.select(groups, function(group){
+        return group.course_id.toString() === selected_course_id;
+      })
+      _.each(groups_of_course, function(group){
+        group_select[0].appendChild(new Option(group.values[0], group.values[1].toString()))
+      })
+    }
 
-  emptyGroups();
-  fillGroups($("#course_select")[0].value);
-
-  $("#course_select").on("change", function(e){
     emptyGroups();
-    var selected_course_id = e.target.selectedOptions[0].value;
-    fillGroups(selected_course_id);
-  })
+    fillGroups($("#course_select")[0].value);
+
+    $("#course_select").on("change", function(e){
+      emptyGroups();
+      var selected_course_id = e.target.selectedOptions[0].value;
+      fillGroups(selected_course_id);
+    })
+  }
 })
