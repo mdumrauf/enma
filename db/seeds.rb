b3d3cd66ac48eb48d8798cac832d9263a059f694
Matias Dumrauf <matias.dumrauf@gmail.com>
2015-03-30 20:16:17 -0300

    added seeds for courses and groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Four courses. 15 groups per course.
+
+courses = Course.create([{ code: 'Miércoles Mañana' }, { code: 'Miércoles Noche' }, { code: 'Jueves Mañana' },  { code: 'Viernes Noche' }])
+
+15.times{ |n| Group.create(:course => Course.first,  :name => n + 1) }
+15.times{ |n| Group.create(:course => Course.second,  :name => n + 1) }
+15.times{ |n| Group.create(:course => Course.third,  :name => n + 1) }
+15.times{ |n| Group.create(:course => Course.fourth,  :name => n + 1) }
