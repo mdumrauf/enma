8eccceb0710581842d4825d36aa6951233fde23e
Matias Dumrauf <matias.dumrauf@gmail.com>
2015-03-31 22:04:12 -0300

    amend

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,16 +2,22 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   before_filter :create
 
   def create
-    @client = Github.new(:login => "user", :password => "pass", :org => "dds-utn")
+    @client = Github.new(:login => "mdumrauf", :password => "matiiar50zx0o", :org => "dds-utn")
     @teams  = @client.organizations.teams.list("dds-utn")
+    @teams.each { |e| logger.info e }
   end
 
   def add_user(team_name, user)
     team = @teams.find { |e| e['name'] == team_name }
     if team != nil
-      @client.organizations.teams.add_team_member(team.id, user) unless user.strip.length == 0
+      begin
+        @client.organizations.teams.add_member(team.id, user) unless user.strip.length == 0
+      rescue Exception => e
+        $logger.error e.message
+        $logger.error e.backtrace.inspect
+      end
     else
-      print "Team: #{team_name} is invalid"
+      logger.error "Team: #{team_name} is invalid"
     end
   end
 
@@ -20,6 +26,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
 
+      logger.info "=========== TRUE =============="
       group_code = Group.find_by(id: @user.group_id).code
       course_code = Course.find_by(id: @user.course_id).code
       team_name = "2015-#{course_code}-group-#{group_code}"
@@ -29,6 +36,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
     else
+      logger.info "=========== FALSE =============="
       session["devise.github_data"] = request.env["omniauth.auth"].except("extra").merge(password: @user.password)
       redirect_to new_user_registration_url
     end
