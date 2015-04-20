9d53209a1df0c8f7e6cbbf9d8a4c90e80f05e340
Matias Dumrauf <matias.dumrauf@gmail.com>
2015-04-20 20:12:18 -0300

    made group_id and course_id attr_accesible

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:github]
 
-  attr_accessible :name, :file_number, :email, :provider, :uid
+  attr_accessible :name, :file_number, :email, :provider, :uid, :group_id, :course_id
 
 
   validates :name, presence: true
